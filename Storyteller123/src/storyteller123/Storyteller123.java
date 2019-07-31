53affdabd1a0208a1cff29cf79699e2758ee6a96
diogoxdxd <53489281+diogoxdxd@users.noreply.github.com>
2019-07-31 12:01:56 -0300

    Update Storyteller123.java

--- Storyteller123/src/storyteller123/Storyteller123.java
+++ Storyteller123/src/storyteller123/Storyteller123.java
@@ -5,19 +5,21 @@ public class Storyteller123 {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
+	    
+	//comeca intro
+        Narrador.getIntro();
+        Pessoa jogador = new Pessoa();
+        
+	    jogador.setNome(Caixa.retorna("Qual o seu primeiro nome?\n\n"));
+            jogador.setIdade(Integer.parseInt(Caixa.retorna("Qual sua idade?\n\n")));
 
-         Textos t = new Textos();
-         Introducao intro = new Introducao();
-       
-        System.out.println( intro.setGetNarracao());
-
-
+    	//comeca historia
 		Historia historia = new Historia(5);
                
-		Evento e1 = new Evento(t.getTextos(), "Olho o corpo", "Vou embora");
-		Evento e2 = new Evento(t.getTextos(), "copo", "piano", "veneno");
-		Evento e3 = new Evento(t.getTextos(), "c ta maluco", "oloco meu", "oloquinho meu");
-		Evento e4 = new Evento(t.getTextos(), "botao 1", "botao 2", "botao 3");
+		Evento e1 = new Evento(Textos.getTextos(), "Olho o corpo", "Vou embora");
+		Evento e2 = new Evento(Textos.getTextos(), "copo", "piano", "veneno");
+		Evento e3 = new Evento(Textos.getTextos(), "c ta maluco", "oloco meu", "oloquinho meu");
+		Evento e4 = new Evento(Textos.getTextos(), "botao 1", "botao 2", "botao 3");
 
 		
 		historia.addHistoria(e1);
