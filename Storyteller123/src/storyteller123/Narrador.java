bbfe6012edd26636bef85412ce9e680599672cfa
diogoxdxd <53489281+diogoxdxd@users.noreply.github.com>
2019-07-31 11:54:01 -0300

    Add files via upload

---
+++ Storyteller123/src/storyteller123/Narrador.java
@@ -0,0 +1,20 @@
+package storyteller;
+
+import javax.swing.JOptionPane;
+
+public class Narrador{
+	
+            public Pessoa jogador;
+    
+    
+            public static void getIntro() {
+                    Caixa.messageOk(Textos.startIntro());		
+            }
+	
+            public Pessoa getPlayer() {
+                        this.jogador.getNome();
+                        this.jogador.getIdade();
+                        return jogador;	
+            }
+
+}
