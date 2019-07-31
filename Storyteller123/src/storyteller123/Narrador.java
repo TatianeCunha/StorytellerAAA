501df8e25bffb5686aeb675b5aa4182b404f615d
diogoxdxd <53489281+diogoxdxd@users.noreply.github.com>
2019-07-31 12:51:09 -0300

    Update Narrador.java

--- Storyteller123/src/storyteller123/Narrador.java
+++ Storyteller123/src/storyteller123/Narrador.java
@@ -4,17 +4,18 @@ import javax.swing.JOptionPane;
 
 public class Narrador{
 	
-            public Pessoa jogador;
+          //  public Pessoa jogador;
     
     
             public static void getIntro() {
                     Caixa.messageOk(Textos.startIntro());		
             }
-	
+	/*
             public Pessoa getPlayer() {
                         this.jogador.getNome();
                         this.jogador.getIdade();
                         return jogador;	
             }
+	*/
 
 }
